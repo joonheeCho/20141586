071d51a263cab11cb1df456240a5219af15861d9
joonheeCho <tlatlago8282@gmail.com>
2019-03-18 13:52:36 +0900

    Initial Commit

---
+++ 20141586/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20141586
+//
+//  Created by Release on 2019. 3. 18..
+//  Copyright © 2019년 joonhee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
